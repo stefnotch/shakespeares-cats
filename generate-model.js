5c77664a6970bf02914563d90e52947c2140fc88
Stefnotch <brandmairstefan@gmail.com>
2019-12-09 17:56:36 +0100

    Fix Markov model generation

--- generate-model.js
+++ generate-model.js
@@ -1,16 +1,27 @@
 const Markov = require("compiled-markov");
 const fs = require("fs");
-const sourceFileName = "./assets/shakespeare_hamlet.txt";
+const sourceFileName = "./assets/shakespeare_sonnets.txt";
 const outputFileName = "./assets/compiled.json";
 
 let allLines = [];
 fs.readFile(sourceFileName, "utf8", (error, data) => {
   if (error) throw error;
 
-  const mark = new Markov();
-  mark.initFromText(data);
+  let lines = data.split("\n");
+  let cleanedUpData = "";
+  lines.forEach(line => {
+    line = line.replace("\r", "").replace(/^\s+/g, "");
+    if (line.length <= 0) return;
 
-  console.log(mark.generateSentences(1));
+    cleanedUpData += line.replace(/[,:;]$/g, "") + ". ";
+  });
+
+  const mark = new Markov();
+  mark.initFromText(cleanedUpData);
+
+  console.log(mark.generateSentences(1, ""));
+  console.log(mark.generateSentences(1, ""));
+  console.log(mark.generateSentences(1, ""));
 
   fs.writeFile(outputFileName, mark.getCompiled(), "utf8", error => {
     if (error) throw error;
